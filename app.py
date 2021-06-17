d59feaa6d2ce7e5d18c25e7ce78ab4a5cd91810c
ibiyutsuki <67356328+ibiyutsuki@users.noreply.github.com>
2021-06-17 10:38:46 +0900

    Revert "四角系の計算昨日追加"

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template,request
-from flask.templating import render_template_string
-from werkzeug.wrappers import Request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,17 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def top_page():
     return render_template("index.html")
-
-@app.route("/square_input")
-def square_input():
-    return render_template("square_input.html")
-
-@app.route("/square_result")
-def square_result():
-    hei = int(request.args.get("hei"))
-    bot = int(request.args.get("bot"))
-    result = hei * bot
-    return render_template("square_result.html",result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
