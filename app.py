a9a950454fca343f79983fc00d2bedbbf3b0a9bd
ibi yutsuki <y.ibi.sys20@morijyobi.ac.jp>
2021-06-17 10:29:24 +0900

    四角系の計算昨日追加

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask.templating import render_template_string
+from werkzeug.wrappers import Request
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route("/")
 def top_page():
     return render_template("index.html")
+
+@app.route("/square_input")
+def square_input():
+    return render_template("square_input.html")
+
+@app.route("/square_result")
+def square_result():
+    hei = int(request.args.get("hei"))
+    bot = int(request.args.get("bot"))
+    result = hei * bot
+    return render_template("square_result.html",result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
